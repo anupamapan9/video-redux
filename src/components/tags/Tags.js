f9fea3aca5f1b549b7b4b808bf81a01e70135bae
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-08-31 20:52:42 +0600

    added tag in the ui 6.8

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Tag from './Tag';
 
+import { fetchTags } from '../../features/tags/tagsSlice';
+
 const Tags = () => {
+    const { tags, loading } = useSelector(state => state.tags)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchTags())
+    }, [dispatch])
+
     return (
-        <section>
+        tags.length > 0 ? <section>
             <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-                <Tag />
+                {tags.map(tag => <Tag tag={tag} key={tag.id} />)}
             </div>
-        </section>
+        </section> : null
     );
 };
 
